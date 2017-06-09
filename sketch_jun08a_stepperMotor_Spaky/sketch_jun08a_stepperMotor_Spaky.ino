5347cf4590190e6368d616821b415f3a41ec8d1d
vrpronyc <vrpronyc@gmail.com>
2017-06-09 19:42:10 -0400

    Cleaned up sketch
    Cleaned up sketch.
    Looks like we don't have enough pins.
    Using pin 5 causes the system to halt.
    We'll have to use a different arduino board (one that has more than 6 pins)

--- sketch_jun08a_stepperMotor_Spaky/sketch_jun08a_stepperMotor_Spaky.ino
+++ sketch_jun08a_stepperMotor_Spaky/sketch_jun08a_stepperMotor_Spaky.ino
@@ -1,3 +1,6 @@
+#include <DigiKeyboard.h>
+
+
 /*
    BYJ48 Stepper motor code
    Connect :
@@ -12,120 +15,129 @@
      28/9/2013
   */
 
+#define USE_DIGIKEYBOARD
 
-//#define IN1  8
-//#define IN2  9
-//#define IN3  10
-//#define IN4  11
-
+// Specify the DigiSpark pins to use
 #define IN1  0
 #define IN2  1
 #define IN3  2
+#ifdef USE_DIGIKEYBOARD
+#define IN4  5
+#else
 #define IN4  3
-#include "DigiKeyboard.h"
+#endif
 
 int Steps = 0;
 boolean Direction = true; // gre
 unsigned long last_time;
 unsigned long currentMillis ;
-int steps_left=4095;
+int steps_per_shot=128;
+int steps_left=steps_per_shot;
 long time;
 void setup()
 {
-//Serial.begin(115200);
-pinMode(IN1, OUTPUT); 
-pinMode(IN2, OUTPUT); 
-pinMode(IN3, OUTPUT); 
-pinMode(IN4, OUTPUT); 
-// delay(1000);
+  pinMode(IN1, OUTPUT); 
+  pinMode(IN2, OUTPUT); 
+  pinMode(IN3, OUTPUT); 
+  pinMode(IN4, OUTPUT); 
+#ifdef USE_DIGIKEYBOARD
+  DigiKeyboard.delay(1000);
+#else
+  delay(1000);
+#endif
 
 }
 void loop()
 {
-  while(steps_left>0){
-  currentMillis = micros();
-  if(currentMillis-last_time>=1000){
-  stepper(1); 
-  time=time+micros()-last_time;
-  last_time=micros();
-  steps_left--;
+  while(steps_left>0)
+  {
+    currentMillis = micros();
+    if(currentMillis-last_time>=1000)
+    {
+      stepper(1); 
+      time=time+micros()-last_time;
+      last_time=micros();
+      steps_left--;
+    }
   }
-  }
+#ifdef USE_DIGIKEYBOARD
   DigiKeyboard.sendKeyStroke(0);
-  Serial.println(time);
-  Serial.println("Wait...!");
   DigiKeyboard.println("Hello Digispark!");
-  DigiKeyboard.delay(5000);
-  //delay(2000);
+  DigiKeyboard.delay(1000);
+#else
   delay(1000);
-  Direction=!Direction;
-  steps_left=4095;
+#endif
+  steps_left=steps_per_shot;
 }
 
-void stepper(int xw){
-  for (int x=0;x<xw;x++){
-switch(Steps){
-   case 0:
-     digitalWrite(IN1, LOW); 
-     digitalWrite(IN2, LOW);
-     digitalWrite(IN3, LOW);
-     digitalWrite(IN4, HIGH);
-   break; 
-   case 1:
-     digitalWrite(IN1, LOW); 
-     digitalWrite(IN2, LOW);
-     digitalWrite(IN3, HIGH);
-     digitalWrite(IN4, HIGH);
-   break; 
-   case 2:
-     digitalWrite(IN1, LOW); 
-     digitalWrite(IN2, LOW);
-     digitalWrite(IN3, HIGH);
-     digitalWrite(IN4, LOW);
-   break; 
-   case 3:
-     digitalWrite(IN1, LOW); 
-     digitalWrite(IN2, HIGH);
-     digitalWrite(IN3, HIGH);
-     digitalWrite(IN4, LOW);
-   break; 
-   case 4:
-     digitalWrite(IN1, LOW); 
-     digitalWrite(IN2, HIGH);
-     digitalWrite(IN3, LOW);
-     digitalWrite(IN4, LOW);
-   break; 
-   case 5:
-     digitalWrite(IN1, HIGH); 
-     digitalWrite(IN2, HIGH);
-     digitalWrite(IN3, LOW);
-     digitalWrite(IN4, LOW);
-   break; 
-     case 6:
-     digitalWrite(IN1, HIGH); 
-     digitalWrite(IN2, LOW);
-     digitalWrite(IN3, LOW);
-     digitalWrite(IN4, LOW);
-   break; 
-   case 7:
-     digitalWrite(IN1, HIGH); 
-     digitalWrite(IN2, LOW);
-     digitalWrite(IN3, LOW);
-     digitalWrite(IN4, HIGH);
-   break; 
-   default:
-     digitalWrite(IN1, LOW); 
-     digitalWrite(IN2, LOW);
-     digitalWrite(IN3, LOW);
-     digitalWrite(IN4, LOW);
-   break; 
-}
-SetDirection();
-}
+void stepper(int xw)
+{
+  for (int x=0;x<xw;x++)
+  {
+    switch(Steps)
+    {
+       case 0:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, HIGH);
+       break; 
+       case 1:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, HIGH);
+         digitalWrite(IN4, HIGH);
+       break; 
+       case 2:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, HIGH);
+         digitalWrite(IN4, LOW);
+       break; 
+       case 3:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, HIGH);
+         digitalWrite(IN3, HIGH);
+         digitalWrite(IN4, LOW);
+       break; 
+       case 4:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, HIGH);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, LOW);
+       break; 
+       case 5:
+         digitalWrite(IN1, HIGH); 
+         digitalWrite(IN2, HIGH);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, LOW);
+       break; 
+         case 6:
+         digitalWrite(IN1, HIGH); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, LOW);
+       break; 
+       case 7:
+         digitalWrite(IN1, HIGH); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, HIGH);
+       break; 
+       default:
+         digitalWrite(IN1, LOW); 
+         digitalWrite(IN2, LOW);
+         digitalWrite(IN3, LOW);
+         digitalWrite(IN4, LOW);
+       break; 
+    }
+    SetDirection();
+  }
 } 
-void SetDirection(){
-if(Direction==1){ Steps++;}
-if(Direction==0){ Steps--; }
-if(Steps>7){Steps=0;}
-if(Steps<0){Steps=7; }
+void SetDirection()
+{
+  if(Direction==1){ Steps++;}
+  if(Direction==0){ Steps--; }
+  if(Steps>7){Steps=0;}
+  if(Steps<0){Steps=7; }
 }
